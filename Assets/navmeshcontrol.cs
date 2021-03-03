e3602ef8b486414111abc9a2cdd969597383fca4
landox99 <41804607+landox99@users.noreply.github.com>
2021-03-03 14:38:09 -0600

    torreta definitiva y navmesh
    contiene el trabajo de la torreta que dispara y gira ademas del trabajo de la clase anterior que en este caso debido a problemas tecnicos se me cerro y tuve que encontra una forma alternativa por asi decirlo en este caso la capsula esta situada en un escenario con obstaculos y debe encontrar la forma de llegar a una esfera estatica

---
+++ Assets/navmeshcontrol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navmeshcontrol : MonoBehaviour
+{
+    public Transform objetivo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agente = GetComponent<NavMeshAgent>();
+        agente.destination = objetivo.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
